5a45eaa76252df8b551ad2ae3842a81552b3320d
claire-d-b <claire.deberny@essca.eu>
2024-11-16 10:19:41 +0100

    remove try and except

--- estimate.py
+++ estimate.py
@@ -3,40 +3,37 @@ from matplotlib.pyplot import savefig, tight_layout, subplots
 
 
 def main():
-    try:
-        """Program that estimates the price with theta_0 = 0 and theta_1 = 0"""
-        lhs, rhs = get_lists_from_dataframe("data.csv", "km", "price")
+    """Program that estimates the price with theta_0 = 0 and theta_1 = 0"""
+    lhs, rhs = get_lists_from_dataframe("data.csv", "km", "price")
 
-        theta_0 = 0
-        theta_1 = 0
+    theta_0 = 0
+    theta_1 = 0
 
-        print("Please enter a mileage:")
-        mileage = input()
+    print("Please enter a mileage:")
+    mileage = input()
 
-        theta_0, theta_1 = open_thetas_file("thetas.csv")
+    theta_0, theta_1 = open_thetas_file("thetas.csv")
 
-        if int(theta_0 + theta_1 * float(mileage)) > 0:
-            print("Estimated price is", int(theta_0 + theta_1 *
-                  float(mileage)))
-        else:
-            print("The car cannot be sold: its value is 0.")
+    if int(theta_0 + theta_1 * float(mileage)) > 0:
+        print("Estimated price is", int(theta_0 + theta_1 *
+                float(mileage)))
+    else:
+        print("The car cannot be sold: its value is 0.")
 
-        pred = []
-        # print("theta_0 (y-interceipt) is: ", theta_0)
-        # print("theta_1 (slope) is: ", theta_1)
+    pred = []
+    # print("theta_0 (y-interceipt) is: ", theta_0)
+    # print("theta_1 (slope) is: ", theta_1)
 
-        for i, unit in enumerate(lhs):
-            pred.insert(i, unit * theta_1 + theta_0)
+    for i, unit in enumerate(lhs):
+        pred.insert(i, unit * theta_1 + theta_0)
 
-        fig, ax = subplots()
+    fig, ax = subplots()
 
-        ax.plot(lhs, pred)
-        ax.scatter(lhs, rhs)
+    ax.plot(lhs, pred)
+    ax.scatter(lhs, rhs)
 
-        tight_layout()
-        savefig("output")
-    except AssertionError as e:
-        raise AssertionError(f"{e}")
+    tight_layout()
+    savefig("output")
 
 
 if __name__ == "__main__":
